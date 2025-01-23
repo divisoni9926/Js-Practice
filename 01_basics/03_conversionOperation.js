8c16289292ea81581a1547d3610fc210d47d24b0
Divyangi soni <159937432+divisoni9926@users.noreply.github.com>
2025-01-23 11:04:21 +0000

    conversionOpeartion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -27,3 +27,34 @@ let someNumber = 33;
 let stringNumber = String(someNumber);
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+//------------------------------------Operators-------------------------
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2**2);
+console.log(2%2);
+
+
+let str1 ="Hello";
+let str2 = " Divi";
+
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+/* let num1 , num2 , num3;
+
+num1 = num2 = num3 = 2+2; */ 
+
+
+//Prefix and postfix operator
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
