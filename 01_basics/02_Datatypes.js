fb276d643f319bf1e8aff8be3debb44131abfb11
Divyangi soni <159937432+divisoni9926@users.noreply.github.com>
2025-01-23 09:47:51 +0000

    datatypes

---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //Treat all JS code as newer version
+
+// alert("Hello"); //We are using nodejs not browser
+
+let name = "Divi";
+let age = 21;
+let isLoggedIn = false
+
+let state = null;
+
+
+//number = 1 to power 53
+//bigInt = Agr number bhut jyada bada hai to bigInt use krte hai
+//String = ""
+//boolean = true/false
+//null = standalone value... Null is a representataion of empty value
+//Undefined = variable is declared but not initilize the value
+//symbol = unquieness
+
+
+//Object = 
+
+console.log(typeof "Divi")
+
+
+
