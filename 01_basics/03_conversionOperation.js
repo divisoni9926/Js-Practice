e9b24c0fa59fc712a3d70f67b87b0989c7be97c3
Divyangi soni <159937432+divisoni9926@users.noreply.github.com>
2025-01-23 10:28:22 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "55abs";
+
+console.log(typeof score);
+
+let valueInNumber = Number(score) //Score is converted in number
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+//"33" = easily convert in 33
+//"33abc" = NaN
+//true = 1;   false = 0
+
+let isLoggedIn = "Divi";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 = true , 0 = false
+//"" = false
+//"Divi" = true
+
+
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
